0c85d5a0094f54555950d73f1037bde35a2a7d3b
VarchukVladimir <fildes@gmail.com>
2015-01-06 15:38:38 +0200

    changes

--- yajl_test.c
+++ yajl_test.c
@@ -12,172 +12,198 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define YAJL_CALLBACK_RETURN_OK 1
 
-typedef struct {
+typedef enum find_status_t {fs_ok, fs_next, fs_find, fs_skip, fs_err} find_status;
+
+typedef struct context_yajl_t {
 	const char **path;
 	char *return_str;
 	int current_depth;
 	int max_depth;
-	int find_flag;
-} context_t;
-//typedef struct context_t *context;
+	find_status find_status;
+} context_yajl;
 
-struct stream_handler_t{
+typedef struct stream_get_key_handl_t {
 	yajl_handle yajl_handle;
-	context_t *ctx;
-};
-typedef struct stream_handler_t *stream_handler;
+	context_yajl *ctx;
+} stream_get_key_handl;
 
+void free_json_stream_get_key_(stream_get_key_handl *);
 
-static int handle_number(void * ctx, const char * stringVal,
-                           size_t stringLen
-						   )
+static int cb_number(void * context, const char * stringVal, size_t stringLen)
 {
-	context_t *c = (context_t *) ctx;
-    char *str = strndup( (const char *)stringVal, stringLen );
-//    printf ("handle number: %s compare %s, also compare %d with %d\n", str, c->path[c->current_depth], c->current_depth, c->max_depth);
-    if ( !c->find_flag && c->current_depth == c->max_depth )
-    {
-    	c->return_str = str;
-    	c->find_flag = 1;
-    	return yajl_status_client_canceled;
-    }
-	return 1;//yajl_status_ok;
-}
-
-
-static int handle_string(void * ctx, const unsigned char * stringVal,
-                           size_t stringLen
-						   )
-{
-	context_t *c = (context_t *) ctx;
-    char *str = strndup( (const char *)stringVal, stringLen );
-//    printf ("handle string: %s compare %s, also compare %d with %d\n", str, c->path[c->current_depth], c->current_depth, c->max_depth);
-    if ( !c->find_flag && c->current_depth == c->max_depth )
-    {
-    	c->return_str = str;
-    	c->find_flag = 1;
-    	return yajl_status_client_canceled;
-    }
-	return 1;
-}
-
-static int handle_map_key(void * ctx, const unsigned char * stringVal,
-                            size_t stringLen)
-{
-	context_t *c = (context_t *)  ctx;
-	char *str = strndup( (const char *)stringVal, stringLen );
-	printf ("handle map key\n");
-	if (!c->find_flag)
+	context_yajl *ctx = (context_yajl *) context;
+	char *str = strndup((const char *) stringVal, stringLen);
+	if (fs_next == ctx->find_status)
 	{
-		if (c->current_depth <= c->max_depth &&  strncasecmp( c->path[c->current_depth], str, strlen (c->path[c->current_depth]) ) == 0)
+		ctx->return_str = str;
+		ctx->current_depth++;
+		ctx->find_status = fs_ok;
+		return YAJL_CALLBACK_RETURN_OK;//yajl_status_client_canceled;
+	}
+	return YAJL_CALLBACK_RETURN_OK;
+}
+
+static int cb_string(void * context, const unsigned char * stringVal,
+		size_t stringLen)
+{
+	context_yajl *ctx = (context_yajl *) context;
+	if (fs_next == ctx->find_status)
+	{
+		char *str = strndup((const char *) stringVal, stringLen);
+		ctx->return_str = str;
+		ctx->current_depth++;
+		ctx->find_status = fs_ok;
+		return YAJL_CALLBACK_RETURN_OK;//yajl_status_client_canceled;
+	}
+	return YAJL_CALLBACK_RETURN_OK;
+}
+
+static int cb_map_key(void * context, const unsigned char * stringVal,
+		size_t stringLen)
+{
+	context_yajl *ctx = (context_yajl *) context;
+	char *str = strndup((const char *) stringVal, stringLen);
+	if (fs_ok != ctx->find_status)
+	{
+		if (fs_next == ctx->find_status && ctx->current_depth == ctx->max_depth)
 		{
-			c->current_depth++;
+			ctx->current_depth--;
+			ctx->find_status = fs_find;
 		}
+		if (fs_find == ctx->find_status
+				&& strncasecmp(ctx->path[ctx->current_depth], (const char *)stringVal,
+						strlen(ctx->path[ctx->current_depth])) == 0)
+		{
+			ctx->current_depth++;
+		}
+		if (ctx->max_depth == ctx->current_depth)
+			ctx->find_status = fs_next;
 	}
-    free (str);
-    return 1;
+	free(str);
+	return YAJL_CALLBACK_RETURN_OK;
+}
+static int cb_start_map(void * context)
+{
+	context_yajl *ctx = (context_yajl *) context;
+	if (fs_next == ctx->find_status)
+	{
+		ctx->find_status = fs_skip;
+		ctx->current_depth--;
+	}
+	return YAJL_CALLBACK_RETURN_OK;
 }
 
-static char *json_get_stream_key (stream_handler s_hadler, const char *json_buff, size_t json_buff_len)
+static int cb_end_map(void * context)
 {
-	yajl_status y_status;
-	y_status= yajl_parse(s_hadler->yajl_handle, (const unsigned char *) json_buff, json_buff_len);
-	if ( y_status != yajl_status_ok )
-		printf ("Error\n");
-	if (s_hadler->ctx->return_str != NULL)
-	{
-		char *str = s_hadler->ctx->return_str;
-		free_json_stream_get_key (s_hadler);
-		s_hadler = NULL;
-		return str;
-	}
-	else
-		return NULL;
+	context_yajl *ctx = (context_yajl *) context;
+	if ( fs_skip == ctx->find_status)
+		ctx->find_status = fs_find;
+	return YAJL_CALLBACK_RETURN_OK;
 }
+
+static int cb_start_array(void * context)
+{
+	return YAJL_CALLBACK_RETURN_OK;
+}
+
+static int cb_end_array(void * context)
+{
+	return YAJL_CALLBACK_RETURN_OK;
+}
+
 const yajl_callbacks callbacks = {
-	NULL,
-	NULL,
-    NULL,
-    NULL,
-	handle_number,
-    handle_string,
-	NULL,
-	handle_map_key,
-    NULL,
-    NULL,
-	NULL
+	NULL,			/*cb NULL*/
+	NULL,			/*cb boolean*/
+	NULL,			/*cb integer*/
+	NULL,			/*cb double*/
+	cb_number,		/*cb number*/
+	cb_string,		/*cb string*/
+	cb_start_map,	/*cb start_map*/
+	cb_map_key,		/*cb map_key*/
+	cb_end_map,		/*cb end_map*/
+	cb_start_array,	/*cb start_array*/
+	cb_end_array	/*cb end_array*/
 };
 
-stream_handler init_json_stream_get_key (const char **path)
+stream_get_key_handl *init_json_stream_get_key(const char **path)
 {
-	context_t *ctx = malloc( sizeof (context_t) );
-	printf ("%zu, %zu\n", sizeof (context_t), sizeof (struct stream_handler_t));
-	stream_handler s_hadler = malloc( sizeof (struct  stream_handler_t) );
-	int i = 0;
+	context_yajl *ctx = calloc(1, sizeof(context_yajl));
+	stream_get_key_handl *s_handler = calloc(1, sizeof(stream_get_key_handl));
+	int count_max_depth = 0;
 
 	ctx->path = path;
-	for (i = 0; path[i]; i++)
+	for (count_max_depth = 0; path[count_max_depth]; count_max_depth++)
 		;
-	ctx->find_flag = 0;
 	ctx->current_depth = 0;
-	ctx->max_depth = i;
+	ctx->max_depth = count_max_depth;
+	ctx->find_status = fs_find;
 	ctx->return_str = NULL;
-	s_hadler->ctx = ctx;
-	s_hadler->yajl_handle = yajl_alloc(&callbacks, NULL, (void *) s_hadler->ctx );
-    yajl_config( s_hadler->yajl_handle, yajl_allow_comments, 1);
-	return s_hadler;
-}
-void free_json_stream_get_key(stream_handler hanld)
-{
-	free ( hanld->ctx );
-	free (hanld);
+	s_handler->ctx = ctx;
+	s_handler->yajl_handle = yajl_alloc(&callbacks, NULL, (void *) s_handler->ctx);
+	yajl_config(s_handler->yajl_handle, yajl_allow_comments, 1);
+	return s_handler;
 }
 
-int
-main(int argc, char ** argv)
+static char *json_get_stream_key(stream_get_key_handl *s_handler,
+		const char *json_buff, size_t json_buff_len)
+{
+	yajl_status y_status;
+	y_status = yajl_parse(s_handler->yajl_handle,
+			(const unsigned char *) json_buff, json_buff_len);
+	if (y_status != yajl_status_ok)
+		printf("Error\n");
+
+	if (s_handler->ctx->return_str != NULL)
+	{
+		char *str = s_handler->ctx->return_str;
+		free_json_stream_get_key_((stream_get_key_handl *) s_handler);
+		s_handler = NULL;
+		return str;
+	} else
+		return NULL;
+}
+
+void free_json_stream_get_key_(stream_get_key_handl *handl)
+{
+	free(handl->ctx);
+	yajl_free(handl->yajl_handle);
+	free(handl);
+}
+
+int main(int argc, char ** argv)
 
 {
-    static unsigned char fileData[200];
-    yajl_status y_status;
-    size_t rd;
-    int retval = 0;
-	const char* token_xpath[] = {"access", "token", "id", (const char* )0};
-	const char* tenant_xpath[] = {"access", "token", "tenant", "id", (const char* )0};
+	static unsigned char fileData[500];
+	size_t rd;
+	int retval = 0;
+	const char* tenant_xpath[] = { "access", "token", "tenant", "id", (const char*) 0 };
 
-	stream_handler tok_handler = init_json_stream_get_key(token_xpath);
-	stream_handler ten_handler = init_json_stream_get_key(tenant_xpath);
+	stream_get_key_handl *ten_handler = init_json_stream_get_key(tenant_xpath);
 
-    FILE *f = fopen ("/home/volodymyr/auth.json", "r");
-    printf ( "%s find flag = %d\n", tok_handler->ctx->path[0], tok_handler->ctx->find_flag );
-    printf ("start parsing\n");
-    int i = 0;
+	FILE *f = fopen("/home/volodymyr/format_auth.json", "r");
+	printf("start parsing\n");
+	int i = 0;
 
-    while ((rd = fread((void *) fileData, 1, sizeof(fileData) - 1, f)) > 0)
-    {
-        fileData[rd] = 0;
-        char *str1 = NULL;
-        char *str2 = NULL;
+	while ((rd = fread((void *) fileData, 1, sizeof(fileData) - 1, f)) > 0)
+	{
+		char *str2 = NULL;
+		fileData[rd] = 0;
+		if (ten_handler
+				&& (str2 = json_get_stream_key(ten_handler,
+						(const char*) fileData, rd)) != NULL)
+		{
+			ten_handler = NULL;
+			printf("%s\n", str2);
+			free(str2);
+		}
+		if (!ten_handler)
+			break;
+		i++;
+	}
+	printf("eng parsing. Total reads count %d\n", i);
 
-        if ( tok_handler && ( str1 = json_get_stream_key (tok_handler, fileData, rd)) != NULL )
-        {
-        	tok_handler = NULL;
-        	printf ( "_%s_%p\n", str1, tok_handler );
-        }
-        if ( ten_handler && ( str2 = json_get_stream_key (ten_handler, fileData, rd)) != NULL )
-        {
-        	ten_handler = NULL;
-        	printf ( "__%s__%p\n", str2, ten_handler);
-        }
-        if ( !ten_handler && !tok_handler )
-        	break;
-        i++;
-        if (i == 10 )
-        	break;
-        printf ("read %d\n", i);
-    }
-    printf ("eng parsing. reads count %d\n", i);
-
-    return retval;
+	return retval;
 }
 
